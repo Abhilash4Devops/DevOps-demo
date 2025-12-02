fb4d5268d359220de4b4dd9203caceca27583c9f
admin <mabhilash92@gmail.com>
2025-12-03 01:04:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  response.end('Hello Bro, DevOps Pipeline Working Successfully! 🚀');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
